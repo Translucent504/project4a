e5c0673b53c384e631dfc5e8ed8ed3b2e8d04a07
Translucent504 <fleshlime.1@gmail.com>
2020-07-16 16:35:46 +0500

    Added a separate animated bird component

---
+++ src/Bird.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import useWebAnimations from "@wellyshen/use-web-animations";
+import bird1 from './images/birds1.png'
+
+const birdStyle = {
+    height:"200px",
+}
+
+const AnimatedBird = ({ customStyle }) => {
+    const birdAnim = useWebAnimations({
+        keyframes: {
+            transform: ["translateX(0)", "translateX(-100%)"],
+        },
+        timing: {
+            delay: 0, 
+            duration: 1000, 
+            iterations: Infinity, 
+            direction: "normal", 
+            easing: "steps(8,end)", 
+        }
+    });
+
+    setInterval(async () => {
+        const animation = birdAnim.getAnimation()
+        if (animation) {
+            if (animation.playbackRate > 1) {
+                animation.updatePlaybackRate(animation.playbackRate * 0.7)
+            }
+        }
+    }, 1500);
+
+    const handleClick = () => {
+        const animation = birdAnim.getAnimation()
+        animation.updatePlaybackRate(animation.playbackRate * 1.3)
+    }
+
+    return (
+        <div onClick={handleClick} style={{cursor:'pointer', width:"170px", height:"200px", overflow:"hidden"}}>
+            <img src={bird1} alt="" style={birdStyle} ref={birdAnim.ref}/>
+        </div>
+    )
+}
+
+
+export default AnimatedBird
